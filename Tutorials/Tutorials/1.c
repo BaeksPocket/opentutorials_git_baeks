93fd45e6401bcb6cc15bfc3f3f596ae5722a0014
SEUNGHWAN\Baeks <bbbaeks@naver.com>
2017-03-05 14:55:08 +0900

    string 추가

--- Tutorials/Tutorials/1.c
+++ Tutorials/Tutorials/1.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
 int main(){
+	char song[] = "JeongEun";
+	
 	printf("hello world\n");
 	printf("hello song!\n");
 	printf("good");
 
 	printf(" try tutorial \n");
+
+
+	
 	return 0;
 }
